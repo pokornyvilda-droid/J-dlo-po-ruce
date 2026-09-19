ee50a4fcb8667690bba16a9d8365640bf7288f18
pokornyvilda-droid <pokornyvilda@gmail.com>
2026-09-19 10:34:05 +0200

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,31 @@
 export default {
-
   async fetch(request, env) {
+    const url = new URL(request.url);
 
+    // TEST D1
+    if (url.pathname === "/api/db-test") {
+      try {
+        const result = await env.DB
+          .prepare("SELECT 1 AS ok")
+          .first();
+
+        return Response.json({
+          success: true,
+          database: "connected",
+          result
+        });
+      } catch (error) {
+        return Response.json(
+          {
+            success: false,
+            error: String(error)
+          },
+          { status: 500 }
+        );
+      }
+    }
+
+    // Všechno ostatní necháme obsloužit webem
     return env.ASSETS.fetch(request);
-
   }
-
 };
